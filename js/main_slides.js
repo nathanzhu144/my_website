9fffea880ff62c64a585e6828fadb3be42df7f8c
nathanzhu144 <znathan@umich.edu>
2018-09-27 22:26:00 -0400

    added cover picture, finalized cover photo, added links to linkedin and github

--- js/main_slides.js
+++ js/main_slides.js
@@ -1,9 +1,12 @@
 var index = 0;
-roulette();
+roulette("mySlides");
 
-function roulette(){
+//Requires: a class name tag for which class to make a slide show of
+//Modifies: Nothing.
+//Effects:  This function is in charge of the slide show on the main page.
+function roulette(class_name){
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName(class_name);
 
     //Making all of the slides disappear
     for(i = 0; i < x.length; ++i){
@@ -12,7 +15,12 @@ function roulette(){
     
     index++;
 
+    //Displays one of the pictures, depending on index
     x[index % x.length].style.display = "block";
-    x[(index + 1) % x.length].style.display = "block";
+
+    //Testing out printing of two pictures at once
+    //x[(index + 3) % x.length].style.display = "block";
+
+    //Every 4 seconds, call function again
     setTimeout(roulette, 4000);
 }
